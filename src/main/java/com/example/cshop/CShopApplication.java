f50e26533e00f7fa96c78dac9de1ace91c411a64
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2025-11-01 18:57:13 +0100

    first commit

---
+++ src/main/java/com/example/cshop/CShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cshop/CShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
