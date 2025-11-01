4b84b84e4dfa2b3c4f819d1d87df116d611a2e69
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2025-11-01 19:17:29 +0100

    one more time

---
+++ src/main/java/com/example/cshop/models/User.java
@@ -0,0 +1,19 @@
+package com.example.cshop.models;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
